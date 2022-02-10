42a96c61148a2ae4b1287dbde3c3a554e1fc8c7f
kahmariano <kmariano138@gmail.com>
2022-02-10 20:20:27 -0300

    contagem de dias

--- main.js
+++ main.js
@@ -3,13 +3,25 @@ const birthday = new Date('2022-08-19')
 
 var interval = birthday.getTime() - today.getTime()
 
-function msToHMS(ms) {
-  const seg = ms / 1000
-  const dias = Math.floor(seg / (3600 * 24))
-  const horas = Math.floor((seg % (3600 * 24)) / 3600)
-  const min = Math.floor((seg % 3600) * 60)
-  const segs = Math.floor(seg % 60)
-  alert(dias + ' dias ' + horas + ':' + min + ':' + segs)
+function convertMS(milliseconds) {
+  var day, hour, minute, seconds
+  seconds = Math.floor(milliseconds / 1000)
+  minute = Math.floor(seconds / 60)
+  seconds = seconds % 60
+  hour = Math.floor(minute / 60)
+  minute = minute % 60
+  day = Math.floor(hour / 24)
+  hour = hour % 24
+  alert(
+    day +
+      ' dias ' +
+      hour +
+      ' horas' +
+      minute +
+      ' minutos' +
+      seconds +
+      ' segundos'
+  )
 }
 
-msToHMS(interval)
+convertMS(interval)
