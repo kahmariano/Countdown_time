c48b03c42922ca9b0670bb5a40abb7a2250f6282
kahmariano <kmariano138@gmail.com>
2022-02-09 22:23:41 -0300

    js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-var today = Date.now()
+var today = new Date().getTime
 
-var dataFinal = new Date('2022-08-19 00:00:00')
+var dataFinal = new Date('2022-08-19 00:00:00').getTime()
 
-var conversao = 1000 * 60 * 60 * 24 * 60
-var intervalo = (dataFinal - today) / conversao
+var intervalo = dataFinal - today
 
 alert(intervalo)
